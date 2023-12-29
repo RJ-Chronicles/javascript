3fb2cadeded24bfa3faa020252b37f6e5e5e0d90
Rajendra Jondhalekar <88649914+RJ-Chronicles@users.noreply.github.com>
2023-12-29 16:22:13 +0530

    Create NoMoreGenerics.ts

---
+++ NoMoreGenerics.ts
@@ -0,0 +1,25 @@
+type ApiResponse<Data> ={
+  data : Data;
+  isError: boolean;
+}
+
+type UserResponse = ApiResponse<{name: string; age: number}>;
+type BlogResponse = ApiResponse<{blog: string; date : Date}>
+const response :UserResponse  = {
+  data : {
+    name: "Jonathon",
+    age: 30,
+  },
+  isError : false
+}
+
+const blogResponse : BlogResponse = {
+  data : {
+    blog: 'this is the unknown',
+    date: new Date(),
+  },
+  isError: false
+}
+console.log(response.data?.name);
+
+console.log(blogResponse.data.date)
